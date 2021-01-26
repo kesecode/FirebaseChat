385a40bcbf02b31a30bf1217094a65f39205eff1
David Weppler <david.weppler@icloud.com>
2021-01-26 16:00:46 +0100

    Create .gitignore

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by David Weppler on 26.01.21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
