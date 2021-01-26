ae9fed2184ee4fecc922c3f8e47c03faba0f3777
David Weppler <david.weppler@icloud.com>
2021-01-26 17:26:31 +0100

    fixed AuthView

--- FirebaseChat/View/AuthView.swift
+++ FirebaseChat/View/AuthView.swift
@@ -7,29 +7,150 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SignInView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var error: String = ""
     @EnvironmentObject var session: SessionStore
     
-    func getUser() {
-        session.listen()
+    func signIn() {
+        session.signIn(email: email, password: password) { (result, error) in
+            if let error = error {
+                self.error = error.localizedDescription
+            } else {
+                self.email = ""
+                self.password = ""
+            }
+        }
     }
     
     var body: some View {
-        Group {
-            if (session.session != nil) {
-                Text("Welcome back \(session.session!.email ?? "user")")
-                Button(action: session.signOut) {
-                    Text("Sign Out")
-                }
-            } else {
-                AuthView()
+        VStack {
+            Text("Welcome Back!")
+                .font(.system(size: 32, weight: .heavy))
+            
+            Text("Sign in to continue")
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(Color("Gray"))
+            
+            VStack(spacing: 18) {
+                TextField("Email address", text: $email)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color("bg1"), lineWidth: 1))
+                
+                SecureField("Password", text: $password)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color("bg1"), lineWidth: 1))
             }
-        }.onAppear(perform: getUser)
+            .padding(.vertical, 64)
+            
+            Button(action: signIn) {
+                Text("Sign in")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14, weight: .bold))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color("bg1"), Color("bg2")]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(5)
+            }
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: SignUpView()) {
+                HStack {
+                    Text("I'm a new user.")
+                        .font(.system(size: 14, weight: .light))
+                        .foregroundColor(.primary)
+                    
+                    Text("Create an account")
+                        .font(.system(size: 14, weight: .semibold))
+                        .foregroundColor(Color("bg2"))
+                }
+            }
+        }
+        .padding(.horizontal, 32)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SignUpView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var error: String = ""
+    @EnvironmentObject var session: SessionStore
+    
+    func signUp() {
+        session.signUp(email: email, password: password) { (result, error) in
+            if let error = error {
+                self.error = error.localizedDescription
+            } else {
+                self.email = ""
+                self.password = ""
+            }
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Create Account")
+                .font(.system(size: 32, weight: .heavy))
+            
+            Text("Sign up to get started")
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(Color("Gray"))
+            
+            VStack(spacing: 18) {
+                TextField("Email address", text: $email)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color("bg1"), lineWidth: 1))
+                
+                SecureField("Password", text: $password)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color("bg1"), lineWidth: 1))
+            }.padding(.vertical, 64)
+            
+            Button(action: signUp) {
+                Text("Create Account")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14, weight: .bold))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color("bg1"), Color("bg2")]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(5)
+            }
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Spacer()
+        }.padding(.horizontal, 32)
+    }
+}
+
+struct AuthView: View {
+    var body: some View {
+        NavigationView {
+            SignInView()
+        }
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environmentObject(SessionStore())
+        AuthView().environmentObject(SessionStore())
     }
 }
