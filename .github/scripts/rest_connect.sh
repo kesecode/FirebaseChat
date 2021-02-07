cd793b1205abd98803b43688fd83b25252100363
David <43249973+kesecode@users.noreply.github.com>
2021-02-07 17:52:48 +0100

    Update rest_connect.sh

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-sudo apt-get install jq
+brew install curl
+brew install jq
+
+COVERAGE=`jq -r '.lineCoverage' result.json`
 
 TOKEN=$(curl --location --request POST 'rest.kesecode.io/api/auth' --header 'Authorization: test@user.de abcabc' | jq -r '.token')
 
