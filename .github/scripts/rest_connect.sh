e65c6ca72bba578bbf662c0b44f3b36fbb75d21c
David <43249973+kesecode@users.noreply.github.com>
2021-02-07 23:13:50 +0100

    Update rest_connect.sh

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-COVERAGE=`jq -r '.lineCoverage' result.json`
-
 TOKEN=$(curl --location --request POST 'rest.kesecode.io/api/auth' --header 'Authorization: test@user.de abcabc' | jq -r '.token')
 
 curl --location --request POST 'rest.kesecode.io/api/post' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"coverage": '$COVERAGE'}'
