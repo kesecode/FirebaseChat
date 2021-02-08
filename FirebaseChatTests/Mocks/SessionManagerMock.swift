ca356fad12dd85f0fb4511043d9df032e13777d8
David Weppler <david.weppler@icloud.com>
2021-02-08 01:35:23 +0100

    Formatting + no strict mode for SwiftLint

--- FirebaseChatTests/Mocks/SessionManagerMock.swift
+++ FirebaseChatTests/Mocks/SessionManagerMock.swift
@@ -7,10 +7,8 @@
 
 @testable import FirebaseChat
 
-
 final class SessionManagerMock: SessionManaging {
     var loginResult: Result<Void, Error> = .success(())
-    
     func login(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
         completion(loginResult)
     }
