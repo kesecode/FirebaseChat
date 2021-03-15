e4fce39dcac95a63d89d7e8c8bbfd3d214191417
David <43249973+kesecode@users.noreply.github.com>
2021-03-16 00:02:11 +0100

    Some CI fixes (#19)
    * Develop (#16)
    
    * hook pre recieve
    
    * Update rest_connect.sh
    
    * Update rest_connect.sh
    
    * Update rest_connect.sh
    
    * Update rest_connect.sh
    
    * Update README.md
    
    * fix rest_connect
    
    * Update rest_connect.sh

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -4,6 +4,6 @@ COVERAGE=`jq -r '.lineCoverage' result.json`
 COVERAGE=$(echo "scale=2; $COVERAGE * 100" | bc)
 COVERAGE=$(echo ${COVERAGE%.*})
 
-TOKEN=$(curl --location --request POST 'rest.kesecode.io/api/auth' --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
+TOKEN=$(curl --location --request POST 'rest.kesecode.io/auth' --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
 
 curl --location --request POST 'rest.kesecode.io/post/kesecode/FirebaseChat/coverage' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"value": '$COVERAGE'}'
