be2bd474694f78f75fc2a71139f3d3c47cb0cca0
David <43249973+kesecode@users.noreply.github.com>
2021-03-15 19:00:34 +0100

    Update rest_connect.sh

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -6,4 +6,4 @@ COVERAGE=$(echo ${COVERAGE%.*})
 
 TOKEN=$(curl --location --request POST 'rest.kesecode.io/api/auth' --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
 
-curl --location --request POST 'rest.kesecode.io/api/post' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"coverage": '$COVERAGE'}'
+curl --location --request POST 'rest.kesecode.io//post/kesecode/FirebaseChat/coverage' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"coverage": '$COVERAGE'}'
